7adbb0dcb7689699e876e43034ec25ec525ae083
karthik <karthickambani007@gmail.com>
2023-08-09 20:44:25 +0530

    change

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,7 @@ import './src/styles/global.css'
 
 import React from 'react'
 
-import IndexPage from './src/pages'
+import IndexPage from './src/pages/index'
 
 export const wrapRootElement = ({element}) => {
     return <IndexPage>{element}</IndexPage>;
